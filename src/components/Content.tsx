c3c95a2c26d1c8dd19e5c7785e68a77343c1ca14
Igor Matias <igorhmatias@gmail.com>
2021-03-17 15:05:32 -0300

    feat: componentização do Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,30 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard, MovieCardProps } from '../components/MovieCard';
+import { GenreResponseProps } from '../App'
+import '../styles/content.scss';
+
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps[];
+  movies: MovieCardProps[];
+}
+
+
+
+export function Content({selectedGenre,movies}: ContentProps)  {
+return (
+  <div className="container">
+  <header>
+    <span className="category">Categoria:<span>{selectedGenre.title}</span></span>
+  </header>
+
+  <main>
+    <div className="movies-list">
+      {movies.map(movie => (
+        <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+</div>)
+  
+
 }
