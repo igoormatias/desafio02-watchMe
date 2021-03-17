caa16e502e12e898f52345a865361dcf6ba9cc37
Igor Matias <igorhmatias@gmail.com>
2021-03-17 15:06:10 -0300

    feat: componentização do Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+import { GenreResponseProps} from '../App'
+
+import '../styles/sidebar.scss';
+
+interface  SidebarProps { 
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  handleClickButton: (genreId: number) => void
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleClickButton
+}: SidebarProps) {
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
+  
 }
